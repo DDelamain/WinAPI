941b49434f240452d1ad44a876a9725ac5a2e4db
dando <danila.dor.1125@mail.ru>
2025-12-23 22:53:01 +0500

    Login invitation DONE

--- DialogBox/main.cpp
+++ DialogBox/main.cpp
@@ -1,8 +1,11 @@
 ﻿#include<Windows.h>
 #include"resource.h"
+
+CONST CHAR g_sz_INVITE[] = "Введите имя пользователя";//на паре
+
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow) 
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
 	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
 	return 0;
@@ -16,40 +19,67 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
-		SetDlgItemTextA(hwnd, IDC_EDIT_LOGIN, "Введите имя пользователя");
+		//SetDlgItemTextA(hwnd, IDC_EDIT_LOGIN, "Введите имя пользователя");//сам
+		HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);//на паре
+		SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)g_sz_INVITE);//на паре
 	}
-		break;
+	break;
 	case WM_COMMAND:	//В этой секции обрабатываются нажатия кнопок, клавиш и другие события
-	{
-		if (LOWORD(wParam) == IDC_EDIT_LOGIN){//Проверяем, что выбрана именно строка с логином
+	//мое самостоятельное решение
+	/* {
+		if (LOWORD(wParam) == IDC_EDIT_LOGIN) {//Проверяем, что выбрана именно строка с логином
 			if (HIWORD(wParam) == EN_SETFOCUS)//Проверка, что возвращаемое значение - это EN_SETFOCUS
 			{
 				SetDlgItemTextA(hwnd, IDC_EDIT_LOGIN, "");
 			}
+			if (HIWORD(wParam) == EN_KILLFOCUS)
+			{
+				HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+				int textLength = GetWindowTextLength(hEditLogin);
+				if (textLength == 0)
+				{
+					SetWindowText(hEditLogin, "Введите имя пользователя");
+				}
+				//SetDlgItemTextA(hwnd, IDC_EDIT_LOGIN, "Введите имя пользователя");
+			}
+			
 		}
-	}
+	}*/
+	switch (LOWORD(wParam))
 	{
-		if (HIWORD(wParam) == EN_KILLFOCUS)
+	case IDC_EDIT_LOGIN://на паре
+	{
+		CONST INT SIZE = 256;//на паре
+		CHAR sz_buffer[SIZE] = {};//на паре
+		HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);//на паре
+		SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);// на паре
+
+		if (HIWORD(wParam) == EN_SETFOCUS)//на паре
 		{
-			SetDlgItemTextA(hwnd, IDC_EDIT_LOGIN, "Введите имя пользователя");
+			if (strcmp(sz_buffer, g_sz_INVITE) == 0)//на паре
+				SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)"");//на паре
+		}
+		if (HIWORD(wParam) == EN_KILLFOCUS)//на паре
+		{
+			if (strcmp(sz_buffer, "") == 0)//на паре
+				SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)g_sz_INVITE);//на паре
 		}
 	}
-		switch (LOWORD(wParam))
-		{
-		case IDC_BUTTON_COPY:
-		{
-			CONST INT SIZE = 256;
-			CHAR sz_buffer[SIZE] = {};
-			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
-			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
-			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
-			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
-		}
-		break;
-		case IDOK: MessageBox(hwnd, "Была нажата кнопка ОК", "Info", MB_OK | MB_ICONINFORMATION); break;
-		case IDCANCEL: EndDialog(hwnd, 0); break;
-		}
-		break;
+	break;
+	case IDC_BUTTON_COPY:
+	{
+		CONST INT SIZE = 256;
+		CHAR sz_buffer[SIZE] = {};
+		HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+		HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
+		SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+		SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+	}
+	break;
+	case IDOK: MessageBox(hwnd, "Была нажата кнопка ОК", "Info", MB_OK | MB_ICONINFORMATION); break;
+	case IDCANCEL: EndDialog(hwnd, 0); break;
+	}
+	break;
 	case WM_CLOSE:		//Отрабатывает 1 раз при закрытии окна
 		EndDialog(hwnd, 0);
 		break;
