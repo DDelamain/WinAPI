d2048d8431612dbfe6303ee9226ceed47c7004ce
dando <danila.dor.1125@mail.ru>
2025-12-25 20:23:11 +0500

    added cancel button, icon and OK-button notifficacion

--- ComboBox/main.cpp
+++ ComboBox/main.cpp
@@ -17,6 +17,8 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 	case WM_INITDIALOG:
 	{
+		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
 		HWND hCombo = GetDlgItem(hwnd, IDC_COMBO);
 		for (int i = 0; i < sizeof(g_sz_VALUES) / sizeof(g_sz_VALUES[0]); i++)
 		{
@@ -25,6 +27,22 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	}
 		break;
 	case WM_COMMAND:
+	switch (LOWORD(wParam))
+	{
+	case IDOK: 
+	{
+		HWND hCombo = GetDlgItem(hwnd, IDC_COMBO);
+		int index = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
+		int lenOfText = SendMessage(hCombo, CB_GETLBTEXTLEN, index, 0);
+		TCHAR* buffer = new TCHAR[lenOfText];
+		SendMessage(hCombo, CB_GETLBTEXT, index, (LPARAM)buffer);
+		TCHAR line[256];
+		wsprintf(line, "Был выбран пункт %d %s", index+1, buffer);
+		MessageBox(hwnd, line, "Info", MB_OK | MB_ICONINFORMATION);
+		delete[] buffer; 
+	}
+	case IDCANCEL: EndDialog(hwnd, 0);
+	}
 		break;
 	case WM_CLOSE:
 		EndDialog(hwnd, 0);
