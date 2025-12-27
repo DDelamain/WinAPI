087b89c6079e8f8fe47a97c35004388227a5e0bb
dando <danila.dor.1125@mail.ru>
2025-12-27 22:44:42 +0500

    ListBoxAdd DONE

--- ListBox/main.cpp
+++ ListBox/main.cpp
@@ -1,9 +1,11 @@
+#undef UNICODE
 #include<Windows.h>
 #include"resource.h"
 
 CONST CHAR* g_sz_VALUES[] = { "This", "is","my","first", "List","Box" };
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK DlgProcAdd(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
@@ -29,6 +31,10 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
+		case IDC_BUTTON_ADD: 
+		{
+			DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ADD), hwnd, DlgProcAdd, 0);
+		}
 		case IDOK:
 		{
 
@@ -38,6 +44,45 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		break;
 	case WM_CLOSE:
 		EndDialog(hwnd, 0);
+	}
+	return FALSE;
+}
+
+BOOL CALLBACK DlgProcAdd(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+		SetFocus(GetDlgItem(hwnd, IDC_EDIT_ADD));
+		break;
+	case WM_COMMAND: 
+	{
+		switch (LOWORD(wParam)) 
+		{
+		case IDOK:
+		{
+			CHAR sz_buffer[MAX_PATH] = {};
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_ADD);
+			SendMessage(hEdit, WM_GETTEXT, MAX_PATH, (LPARAM)sz_buffer);
+			HWND hParent = GetParent(hwnd);
+			HWND hList = GetDlgItem(hParent, IDC_LIST);
+			SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)sz_buffer);
+			if (SendMessage(hList, LB_FINDSTRINGEXACT, 0, (LPARAM)sz_buffer) == LB_ERR)
+				SendMessage(hList, LB_FINDSTRING, 0, (LPARAM)sz_buffer);
+			else
+			{
+				MessageBox(hwnd, "Такой элемент уже добавлен", "Info", MB_OK | MB_ICONINFORMATION);
+				break;
+			}
+		}
+			break;
+		case IDCANCEL:EndDialog(hwnd, 0);
+			break;
+		}
+	}
+	break;
+
+	case WM_CLOSE:EndDialog(hwnd, 0);
 	}
 	return FALSE;
 }
